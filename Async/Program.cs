da96cc146627bf110457765087f94913be6afbcf
Anuitex <viktoriamay0130@gmail.com>
2019-02-14 09:55:31 +0200

    Code refactoring

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,15 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Async
 {
     class Program
     {
+        private readonly static TaskService _taskService = new TaskService();
+
         static void Main(string[] args)
         {
+            ExecuteTasks();
+            ExecuteThreads();
+            ExecuteParallels();
+        }
+
+        static void ExecuteTasks()
+        {
+            Task firstTask = Task.Factory.StartNew(() => _taskService.GetFactorial(13));
+            Task secondTask = Task.Factory.StartNew(() => _taskService.GetFactorial(10));
+            Task thirdTask = Task.Factory.StartNew(() => _taskService.GetFactorial(15));
+            Task fourthTask = Task.Factory.StartNew(() => _taskService.GetFactorial(5));
+
+            Task.WaitAll(firstTask, secondTask, thirdTask, fourthTask);
+            Console.WriteLine("All threads are completed");
+
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+
+        static void ExecuteThreads()
+        {
+            Thread firstThread = new Thread(() => _taskService.GetFactorial(13));
+            Thread secondThread = new Thread(() => _taskService.GetFactorial(10));
+            Thread thirdThread = new Thread(() => _taskService.GetFactorial(15));
+            Thread fourthThread = new Thread(() => _taskService.GetFactorial(5));
+
+            firstThread.Start();
+            secondThread.Start();
+            thirdThread.Start();
+            fourthThread.Start();
+
+            firstThread.Join();
+            secondThread.Join();
+            thirdThread.Join();
+            fourthThread.Join();
+            Console.WriteLine("All threads are completed");
+
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+
+        static void ExecuteParallels()
+        {
+            int firstIteration = 11;
+            int lastIteration = 15;
+
+            Parallel.For(firstIteration, lastIteration, iteration => _taskService.GetFactorial(iteration));
+            Console.WriteLine("All threads are completed");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Async/TaskService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Async
+{
+    public class TaskService
+    {
+        public void GetFactorial(int factorial)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+            Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}) started (Now:{DateTime.Now.ToLongTimeString()})");
+            int result = 1;
+            for (int iteration = 1; iteration <= factorial; iteration++)
+            {
+                result *= iteration;
+                Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}) iteration: {iteration} (Now:{DateTime.Now.ToLongTimeString()})");
+                Thread.Sleep(100);
+            }
+
+            timer.Stop();
+            TimeSpan timeSpan = timer.Elapsed;            
+            var elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+                timeSpan.Milliseconds / 10);
+
+            Console.WriteLine($"Thread({Thread.CurrentThread.ManagedThreadId}) Factorial of {factorial} = {result} (Now:{DateTime.Now.ToLongTimeString()}) Elapsed time: {elapsedTime}");
+        }
+    }
+}
